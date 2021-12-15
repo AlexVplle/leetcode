cbe4ac9c518b7b8eba4e3571773ed5fec0823e9f
AlexVplle <48535782+AlexVplle@users.noreply.github.com>
2021-12-15 18:30:24 +0000

    Sync LeetCode submission Runtime - 92 ms (5.77%), Memory - 14 MB (100.00%)

--- 0013-roman-to-integer/solution.py
+++ 0013-roman-to-integer/solution.py
@@ -1,9 +1,18 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        sumNumber, romanNumber = 0, {'CM' : 900, 'CD' : 400, 'XC' : 90, 'XL' : 40, 'IX' : 9, 'IV' : 4, 'M' : 1000, 'D' : 500, 'C' : 100, 'L' : 50, 'X' : 10, 'V' : 5, 'I' : 1}
-        for key, values in romanNumber.items():
-            while s.find(key) != -1:
-                sumNumber += values
-                s = s[:s.find(key)] + s[s.find(key) + len(key):]
-        return sumNumber
-        
+class Solution(object):
+   def romanToInt(self, s):
+      """
+      :type s: str
+      :rtype: int
+      """
+      roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+      i = 0
+      num = 0
+      while i < len(s):
+         if i+1<len(s) and s[i:i+2] in roman:
+            num+=roman[s[i:i+2]]
+            i+=2
+         else:
+            #print(i)
+            num+=roman[s[i]]
+            i+=1
+      return num
