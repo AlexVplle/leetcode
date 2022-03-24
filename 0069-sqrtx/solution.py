eba5998f3abe06940560ef1c14c126ec62b95620
AlexVplle <48535782+AlexVplle@users.noreply.github.com>
2022-03-24 22:04:01 +0000

    Sync LeetCode submission Runtime - 44 ms (19.32%), Memory - 13.8 MB (100.00%)

--- 0069-sqrtx/solution.py
+++ 0069-sqrtx/solution.py
@@ -1,16 +1,8 @@
 class Solution:
-    def binarySearch(self, array : list, target : int) :
-        end = len(array) - 1
-        mid = end // 2
-        if not array : return -1
-        valueMid = array[mid]*array[mid]
-        if valueMid == target: return mid
-        if len(array) == 1 :
-            if array[0]*array[0] < target : return 0
-            return -1
-        if valueMid > target : return self.binarySearch(array[:mid], target)
-        else : return mid + 1 + self.binarySearch(array[mid+1:], target)
-        
     def mySqrt(self, x: int) -> int:
-        return self.binarySearch([i for i in range(46430)], x)
+        xk = 46340
+        while True :
+            xkNew = xk - (xk*xk - x)/(2*xk)
+            if xk - xkNew <= 1 : return int(xkNew)
+            xk = xkNew
 
