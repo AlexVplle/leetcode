61e1d0eaede1f1d48b433830fe61f9ea39081c95
AlexVplle <48535782+AlexVplle@users.noreply.github.com>
2024-09-19 06:07:15 +0000

    Sync LeetCode submission Runtime - 5 ms (0.00%), Memory - 2.2 MB (100.00%)

--- 3522-find-the-power-of-k-size-subarrays-i/solution.rs
+++ 3522-find-the-power-of-k-size-subarrays-i/solution.rs
@@ -1,22 +1,29 @@
 impl Solution {
     pub fn results_array(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        let k_usize: usize = k as usize;
-        let len_results: usize = nums.len() - k_usize + 1;
-        let mut results: Vec<i32> = vec![0; len_results];
+
+        let k_usize = k as usize;
+        let len_results = nums.len() - k_usize + 1;
+        let mut results = Vec::with_capacity(len_results);
+
         for i in 0..len_results {
-            let mut subarray = &nums[i..i + k_usize];
-            let mut contigious: bool = true;
+            let subarray = &nums[i..i + k_usize];
+            let mut contiguous = true;
+
             for j in 1..k_usize {
                 if subarray[j] != subarray[j - 1] + 1 {
-                    contigious = false;
+                    contiguous = false;
+                    break;
                 }
             }
-            if (contigious) {
-                results[i] = subarray[k_usize - 1];
+
+            if contiguous {
+                results.push(subarray[k_usize - 1]);
             } else {
-                results[i] = -1;
+                results.push(-1);
             }
         }
+
         results
     }
 }
+
