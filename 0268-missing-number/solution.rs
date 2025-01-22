492cae78ec7aeac0f007afde32fceb8596c6dec6
AlexVplle <48535782+AlexVplle@users.noreply.github.com>
2025-01-22 21:02:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 2.5 MB (37.50%)

--- 0268-missing-number/solution.rs
+++ 0268-missing-number/solution.rs
@@ -1,11 +1,6 @@
 impl Solution {
     pub fn missing_number(nums: Vec<i32>) -> i32 {
-        let mut result: i32 = 0;
-        let n: usize = nums.len();
-        for index in 0..n {
-            result ^= index as i32;
-            result ^= nums[index];
-        }
-        result ^ n as i32
+        let n: i32 = nums.len() as i32;
+        n * (n + 1) / 2 - nums.into_iter().sum::<i32>()
     }
 }
