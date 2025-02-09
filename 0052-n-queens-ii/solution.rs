0e9dffc1a8d51a94559b458480cd10b27a28db48
AlexVplle <48535782+AlexVplle@users.noreply.github.com>
2025-02-09 11:01:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 2.3 MB (8.70%)

---
+++ 0052-n-queens-ii/solution.rs
@@ -0,0 +1,38 @@
+impl Solution {
+    pub fn total_n_queens(n: i32) -> i32 {
+        let n: u16 = n as u16;
+        let mut cols: u16 = 0;
+        let mut right_diagonals: u16 = 0;
+        let mut left_diagonals: u16 = 0;
+        Self::backtrack(0, &mut cols, &mut right_diagonals, &mut left_diagonals, n)
+    }
+
+    pub fn backtrack(
+        y: u16,
+        cols: &mut u16,
+        right_diagonals: &mut u16,
+        left_diagonals: &mut u16,
+        n: u16,
+    ) -> i32 {
+        if y == n {
+            return 1;
+        }
+        let mut sum: i32 = 0;
+        for x in 0..n {
+            if (*cols & (1 << x))
+                + (*left_diagonals & (1 << (x + y)))
+                + (*right_diagonals & (1 << (n - x - 1 + y)))
+                == 0
+            {
+                *cols |= 1 << x;
+                *left_diagonals |= 1 << (x + y);
+                *right_diagonals |= 1 << (n - x - 1 + y);
+                sum += Self::backtrack(y + 1, cols, right_diagonals, left_diagonals, n);
+                *cols &= !(1 << x);
+                *left_diagonals &= !(1 << (x + y));
+                *right_diagonals &= !(1 << (n - x - 1 + y));
+            }
+        }
+        sum
+    }
+}
