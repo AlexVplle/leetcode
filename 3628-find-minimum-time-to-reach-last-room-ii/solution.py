feae6bf4a267a9a735c5a46f0e587539e90511fc
AlexVplle <48535782+AlexVplle@users.noreply.github.com>
2025-05-08 14:03:48 +0000

    Sync LeetCode submission Runtime - 1665 ms (86.73%), Memory - 84.3 MB (93.20%)

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/solution.py
@@ -0,0 +1,24 @@
+import heapq
+
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        row = len(moveTime)
+        col = len(moveTime[0])
+        distances = [[float('inf') for _ in range(col)] for _ in range(row)]
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        priority_queue = [(0, 0, 0, 1)]
+        while priority_queue:
+            time, x, y, time_move = heapq.heappop(priority_queue)
+            if x == row - 1 and y == col - 1:
+                return time
+            if moveTime[x][y] == -1:
+                continue
+            moveTime[x][y] = -1
+            for dx, dy in directions:
+                new_x, new_y = x + dx, y + dy
+                if 0 <= new_x < row and 0 <= new_y < col and moveTime[new_x][new_y] != -1:
+                    time_n = max(moveTime[new_x][new_y], time)
+                    if distances[new_x][new_y] > time_n + time_move:
+                        distances[new_x][new_y] = time_n + time_move
+                        heapq.heappush(priority_queue, (time_n + time_move, new_x, new_y, time_move % 2 + 1))
+        return distances[row - 1][col - 1]
