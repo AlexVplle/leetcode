1e475723eddf543a634ea8005bd545e59622c1cf
AlexVplle <48535782+AlexVplle@users.noreply.github.com>
2025-07-05 12:48:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (11.59%)

---
+++ 1510-find-lucky-integer-in-an-array/solution.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        counter = Counter(arr)
+        return max((num for num, freq in counter.items() if num == freq), default=-1)
